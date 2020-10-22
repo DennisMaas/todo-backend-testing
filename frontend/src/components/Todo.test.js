62443bd7154a34c56d666927ddfddb2bf5c4d2f1
Fabian Keck <fabian.keck@posteo.de>
2020-10-22 14:34:19 +0200

    Test for Rendering of status and description

--- frontend/src/components/Todo.test.js
+++ frontend/src/components/Todo.test.js
@@ -1,17 +1,40 @@
 //description and status are in the document
-import {description} from "./Todo";
-
+import React from "react";
+import {render} from "@testing-library/react";
+import  Todo  from "./Todo"
 
 
 describe("unit test :: Todo Component", () => {
-    it("show description and status in todo-card", ()  => {
-
+    it("show description in todo-card", ()  => {
     //GIVEN
-    const description
+        const todo ={
+            description: "do this",
+            status: "OPEN"
+        }
+        const {getByText} = render(<Todo {...todo} />);
+
     //WHEN
+        const actual= getByText(/do this/i);
 
     //THEN
-
+        expect(actual).toBeInTheDocument()
     });
+
+    it("show status in todo-card", ()  => {
+        //GIVEN
+        const todo ={
+            description: "do this",
+            status: "OPEN"
+        }
+        const {getByText} = render(<Todo {...todo} />);
+
+        //WHEN
+        const actual= getByText(/open/i);
+
+        //THEN
+        expect(actual).toBeInTheDocument()
+    });
+
+
 
 })
