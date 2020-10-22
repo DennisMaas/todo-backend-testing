d1dd6c52482aa911b9b20e60849ffe7dc80e7e79
DennisMaas <mail@dennismaas.de>
2020-10-22 13:03:55 +0200

    init

---
+++ frontend/src/components/Todo.test.js
@@ -0,0 +1,17 @@
+//description and status are in the document
+import {description} from "./Todo";
+
+
+
+describe("unit test :: Todo Component", () => {
+    it("show description and status in todo-card", ()  => {
+
+    //GIVEN
+    const description
+    //WHEN
+
+    //THEN
+
+    });
+
+})
